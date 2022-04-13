b6bada0996c847fb32ced83cafce05d67b9853e1
p.gomez <p.gomez@datalogic.com.uy>
2022-04-13 00:14:15 -0300

    Se agrega IRepo, Repo y Clase de ParamSistema

---
+++ DataAcces/RepositorioParamSistema.cs
@@ -0,0 +1,51 @@
+﻿using Dominio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcces
+{
+    class RepositorioParamSistema : IRepositorioParamSistema
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ParamSistema GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable GetTipos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(ParamSistema obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTipo(TipoPlanta unTipo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ParamSistema obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dominio/ParamSistema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class ParamSistema
+    {
+        public int IdParam { get; set; }
+        public int TipoDescMin { get; set; }
+        public int TipoDescMax { get; set; }
+        public int PlantaDescMin { get; set; }
+        public int PlantaDescMax { get; set; }
+        public int TasaIVA { get; set; }
+        public int TasaImpuesto { get; set; }
+        public int TasaArancelaria { get; set; }
+        
+        public ParamSistema() { }
+        public ParamSistema(int TipoDescMin, int TipoDescMax, int PlantaDescMin, int PlantaDescMax, int TasaIVA, int TasaImpuesto, int TasaArancelaria)
+        {
+            this.TipoDescMin = TipoDescMin;
+            this.TipoDescMax = TipoDescMax;
+            this.PlantaDescMin = PlantaDescMin;
+            this.PlantaDescMax = PlantaDescMax;
+            this.TasaIVA = TasaIVA;
+            this.TasaImpuesto = TasaImpuesto;
+            this.TasaArancelaria = TasaArancelaria;
+        }
+    }
+}
