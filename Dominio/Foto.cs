acd845b647b402d485ad2477cd92ad915ceccb43
p.gomez <p.gomez@datalogic.com.uy>
2022-04-23 20:04:31 -0300

    GenerarNombreFoto

--- Dominio/Foto.cs
+++ Dominio/Foto.cs
@@ -9,5 +9,18 @@ namespace Dominio
         public int IdFoto { get; set; }
         public string Nombre { get; set; }
         public string imagen { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(Nombre);
+        }
+
+        public string GenerarNombre(string nombre, string tipo, int contador)
+        {
+            contador++;
+            nombre.Replace(" ", "_");
+            nombre = nombre + "_"+ contador + "." + tipo;
+            return nombre;
+        }
     }
 }
