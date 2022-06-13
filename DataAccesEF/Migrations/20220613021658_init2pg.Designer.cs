24d6cb754540dbdf1c396f2924cf8cacae85e24e
p.gomez <p.gomez@datalogic.com.uy>
2022-06-12 23:19:37 -0300

    Nueva migracion con relación NaN entre Compras - ItemCompra - Plantas

--- DataAccesEF/Migrations/20220613021658_init2pg.Designer.cs
+++ DataAccesEF/Migrations/20220613021658_init2pg.Designer.cs
@@ -4,14 +4,16 @@ using DataAccesEF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccesEF.Migrations
 {
     [DbContext(typeof(ViveroContext))]
-    partial class VivieroContextModelSnapshot : ModelSnapshot
+    [Migration("20220613021658_init2pg")]
+    partial class init2pg
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
